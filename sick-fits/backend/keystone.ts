cbcc7f3db0d28a8ec36bfee319c4553a9767d896
Devin Ford <devin.d.ford@gmail.com>
2021-02-23 05:47:57 -0500

    added initial keystone setup, removed sample env

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseUrl = process.env.DATABASE_URL || 'mongodb://localhost/keystone_urbanstyles';
+
+const sessionConfig = {
+  // How long should they stay signed in
+  maxAge: 60 * 60 * 24 * 30,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseUrl,
+    // TODO: Add seeding here
+  },
+  lists: createSchema({
+    // Schema items go ehre
+  }),
+  ui: {
+    // Add access control
+    isAccessAllowed: () => true,
+  },
+  // TODO: add session values here
+});
